1755c1743ad79d4a20fcdbdd39d8528011bdd862
liuxing-github <1725366795@qq.com>
2016-06-01 03:27:23 -0400

    .

---
+++ src/fibonacci_series.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function fibonacci_series(n)
+{
+var arr = [0,1];
+    if(n === 1)
+    {
+        return arr;
+    }
+    else
+    {
+        for(var i = 2 ; i <= n ; i++)
+        {
+            arr.push((arr[i-1])+arr[i-2]);
+        }
+    }
+    return arr;
+}
+
+module.exports = fibonacci_series;
